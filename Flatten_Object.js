72c7b9182ce99aadde2195deec4c1437938bb2b7
Shubhendu Bora <82198522+shubhendu0@users.noreply.github.com>
2023-02-08 21:19:26 +0530

    Update Flatten_Object.js

--- Flatten_Object.js
+++ Flatten_Object.js
@@ -1,34 +1,79 @@
 //Ques: Flatten the given object as follows :-
 
-// input : {
-//   user_name: "Shubhendu",
-//   job: {
-//      role : "coder",
-//      income : 50000,
+// Input :-
+// {
+//   name: 'titan',
+//   job: { 
+//      role: 'coder', 
+//      income: 50000 
+//   },
+//   hobby: [ 
+//      'games', 
+//      'travel' 
+//   ],
+//   place: [ 
+//      { city: 'lucknow' }, 
+//      { state: 'up' } 
+//   ],
+//   profile: { 
+//      clients: [ 
+//         'Joey_Wisozk', 
+//         'Enrique12', 
+//         'Tia.Fay18' 
+//      ] 
 //   }
 // }
 
-// output: {
-//   user_name : "Shubhendu",
-//   job_role : "coder",
-//   job_income : 50000,
+// Output :-
+// {
+//   name: 'titan',
+//   job_role: 'coder',
+//   job_income: 50000,
+//   hobby_0: 'games',
+//   hobby_1: 'travel',
+//   place_0_city: 'lucknow',
+//   place_1_state: 'up',
+//   profile_clients_0: 'Joey_Wisozk',
+//   profile_clients_1: 'Enrique12',
+//   profile_clients_2: 'Tia.Fay18'
 // }
 
-const user = {
-   user_name: "Shubhendu",
-   job: {
+const user={
+    name:"titan",
+    job: {
       role : "coder",
       income : 50000,
-   }
+    },
+    hobby : [
+      "games" , "travel"
+    ],
+    place:[
+        {city: "lucknow"},
+        {state: "up"},
+    ],
+    profile:{
+         "clients":[
+            "Joey_Wisozk",
+            "Enrique12",
+            "Tia.Fay18"
+         ]
+    },
 }
+console.log(user);
 
 const flatten_obj = (user) => {
     let res = {};
     for(let i in user){
-        if( typeof(user[i]) ==='object' && !Array.isArray(user[i]){
+        if( typeof(user[i]) ==='object' && !Array.isArray(user[i])){
             const obj = flatten_obj(user[i]);
             for(let j in obj){
-                res[i+"_"+j]= obj[j];
+                res[i+"_"+j] = obj[j];
+            }
+        }
+        else if(typeof(user[i]) ==='object' && Array.isArray(user[i])){
+            const obj = flatten_obj(user[i]);
+            for(let j in obj){
+                res[i+"_"+j] = obj[j];
             }
         }
         else{
@@ -37,6 +82,5 @@ const flatten_obj = (user) => {
     }
     return res;
 }
-
 const flat = flatten_obj(user);
 console.log(flat);
